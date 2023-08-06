3b0997a238167c35f782743a0376bfb767302d90
Kristina0610 <gaevskaya-2011@mail.ru>
2023-08-06 13:08:05 +0300

    new test about Solutions

---
+++ src/test/java/tests/TestSubsectionsInSolutionSection.java
@@ -0,0 +1,36 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.components.MainPage;
+
+import static com.codeborne.selenide.Selenide.$;
+
+
+public class TestSubsectionsInSolutionSection extends TestBase {
+  MainPage mainPage = new MainPage();
+
+  String section = "Решения";
+
+  @Test
+  @Tag("remote")
+  @Owner("Kristina Gaevskaya")
+  @Epic("Учёба")
+  @Story("Дипломный проект")
+  @DisplayName("Проверка отображения подразделов в секции 'Решения'")
+  @Severity(SeverityLevel.CRITICAL)
+  void AutomatedQaSectionPageTest() {
+    mainPage.openPage()
+            .closeCookie()
+            .sectionMouseOn(section);
+    $("[href=\"/capitalmarkets\"]").shouldHave(Condition.text("Системы трейдинга"));
+    $("[href=\"/risks\"]").shouldHave(Condition.text("Управление рисками"));
+    $("[href=\"/brokerage\"]").shouldHave(Condition.text("Брокерский бизнес"));
+    $("[href=\"/treasury\"]").shouldHave(Condition.text("Казначейство"));
+    $("[href=\"/loan\"]").shouldHave(Condition.text("Кредитный конвейер"));
+
+  }
+}
