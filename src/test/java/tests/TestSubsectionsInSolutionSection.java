1943cc112557cf9c24c9ba2d4a1e36684765f3c3
Kristina0610 <gaevskaya-2011@mail.ru>
2023-08-06 13:19:34 +0300

    new test about Solutions

--- src/test/java/tests/TestSubsectionsInSolutionSection.java
+++ src/test/java/tests/TestSubsectionsInSolutionSection.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.components.MainPage;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 
@@ -27,7 +28,7 @@ public class TestSubsectionsInSolutionSection extends TestBase {
             .closeCookie()
             .sectionMouseOn(section);
     //$("[href=\"/capitalmarkets\"]").shouldHave(Condition.text("Системы трейдинга"));
-    $(".t-menusub__menu_bottom > div:nth-child(1) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)").shouldHave(Condition.text("Системы трейдинга"));
+    $("[href=\"/capitalmarkets\"]").shouldBe(visible).shouldHave(Condition.text("Системы трейдинга"));
     $("[href=\"/risks\"]").shouldHave(Condition.text("Управление рисками"));
     $("[href=\"/brokerage\"]").shouldHave(Condition.text("Брокерский бизнес"));
     $("[href=\"/treasury\"]").shouldHave(Condition.text("Казначейство"));
